e02ebe1b8b5428e53b8cb387a678170b4f3f9370
Tomeu Sabater <106184541+TomeuSabater@users.noreply.github.com>
2025-01-27 09:11:08 +0100

    Update E_Strings.py

--- PythonTutorial/E_Strings.py
+++ PythonTutorial/E_Strings.py
@@ -12,7 +12,7 @@ Python Strings
 
 #-- How to  the characters ' and " in strings 
 print("#-- Scape symbols in strings")
-print("Hello it's a good new")  # You can write ' between "
+print("Hello it's a good news .... add code to generate a conflict with GIT")  # You can write ' between "
 print('My name starts with "T"') # You can write " between '
 print("You can print \"") # You can scape a symbol using \
 print("You can scape with the \"\\\" symbol")
